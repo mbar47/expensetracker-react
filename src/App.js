3df7ffe7de299f652611b3d9250c580bbbf4e275
micke <micke@pop-os.localdomain>
2020-10-05 18:08:52 +0200

    changes to base UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from "react";
-import { Header } from "./components/Header";
+import React, { useState } from "react";
+import { SwitchButton } from "./components/SwitchButton";
 import { IncomeExpenses } from "./components/IncomeExpenses";
 import { Balance } from "./components/Balance";
 import { TransactionList } from "./components/TransactionList";
 import { AddTransaction } from "./components/AddTransaction";
+import { Graphs } from "./components/Graphs";
 
 import { GlobalProvider } from "./context/GlobalState";
 
 import "./App.css";
 
 function App() {
+  const [displayGraph, setDisplayGraph] = useState(false);
+
   return (
     <GlobalProvider>
       <div className="App">
-        <Header />
-        <div className="container">
-          <Balance />
-          <IncomeExpenses />
-          <TransactionList />
-          <AddTransaction />
-        </div>
+        {displayGraph ? (
+          <>
+            <Balance />
+            <IncomeExpenses />
+            <Graphs />
+            <SwitchButton
+              displayGraph={displayGraph}
+              setDisplayGraph={setDisplayGraph}
+            />
+          </>
+        ) : (
+          <>
+            <Balance />
+            <TransactionList />
+            <SwitchButton
+              displayGraph={displayGraph}
+              setDisplayGraph={setDisplayGraph}
+            />
+            <AddTransaction />
+          </>
+        )}
       </div>
     </GlobalProvider>
   );
