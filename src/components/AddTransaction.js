6ab687ffac6497564b27f2fd7000f61bdb35e94d
micke <micke@pop-os.localdomain>
2020-10-05 18:06:36 +0200

    added collapse feature

--- src/components/AddTransaction.js
+++ src/components/AddTransaction.js
@@ -1,10 +1,13 @@
 import React, { useState, useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
-  const [category, setCategory] = useState("other");
+  const [category, setCategory] = useState("Other");
+  const [hideTrans, setHideTrans] = useState(false);
 
   const { addTransaction } = useContext(GlobalContext);
 
@@ -18,50 +21,60 @@ export const AddTransaction = () => {
       category: category,
     };
 
-    addTransaction(newTransaction);
+    if (text === "" || amount === 0) {
+      alert("Transaction requires all fields to be filled!");
+    } else {
+      addTransaction(newTransaction);
+    }
   };
 
+  const hideTransaction = () => setHideTrans(!hideTrans);
   return (
     <div>
-      <h3>Add transaction</h3>
-      <form onSubmit={onSubmit} action="" id="form">
-        <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input
-            type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-            id="text"
-            placeholder="enter text..."
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="amount">Amount</label>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-            id="amount"
-            placeholder="enter amount"
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="category">Category</label>
-          <select
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            id="category"
-          >
-            <option value="Other">Other</option>
-            <option value="Income">Income</option>
-            <option value="Food">Food</option>
-            <option value="Junk">Junk</option>
-            <option value="Hobby">Hobby</option>
-            <option value="Housing cost">Housing cost</option>
-          </select>
-        </div>
-        <button className="btn">Add transaction</button>
-      </form>
+      <h3 onClick={hideTransaction}>
+        Add transaction{" "}
+        <FontAwesomeIcon icon={hideTrans ? faAngleUp : faAngleDown} />
+      </h3>
+      {!hideTrans ? (
+        <form onSubmit={onSubmit} action="" id="form">
+          <div className="form-control">
+            <label htmlFor="text">Text</label>
+            <input
+              type="text"
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              id="text"
+              placeholder="enter text..."
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="amount">Amount</label>
+            <input
+              type="number"
+              value={amount}
+              onChange={(e) => setAmount(e.target.value)}
+              id="amount"
+              placeholder="enter amount"
+            />
+          </div>
+          <div className="form-control">
+            <label htmlFor="category">Category</label>
+            <select
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              id="category"
+            >
+              <option value="Other">Other</option>
+              <option value="Income">Income</option>
+              <option value="Food">Food</option>
+              <option value="Junk">Junk</option>
+              <option value="Hobby">Hobby</option>
+              <option value="Housing cost">Housing cost</option>
+            </select>
+          </div>
+          <button className="btn">Add transaction</button>
+        </form>
+      ) : null}
     </div>
   );
 };
--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,19 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Transaction } from "./Transaction";
+import { faAngleDown, faAngleUp } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import { GlobalContext } from "../context/GlobalState";
+
 
 export const TransactionList = () => {
   const { transactions } = useContext(GlobalContext);
 
+  const [hideHistory, setHideHistory] = useState(false);
+
+  const handleHideHistory = (e) => setHideHistory(!hideHistory);
+
   return (
     <div>
-      <h3>History</h3>
-      <ul className="list">
-        {transactions.map((transaction) => (
-          <Transaction key={transaction.id} transaction={transaction} />
-        ))}
-      </ul>
+      <h3 onClick={(e) => handleHideHistory(e)}>
+        History <FontAwesomeIcon icon={hideHistory ? faAngleUp : faAngleDown} />
+      </h3>
+      {!hideHistory ? (
+        <ul className="list">
+          {transactions.map((transaction) => (
+            <Transaction key={transaction.id} transaction={transaction} />
+          ))}
+        </ul>
+      ) : null}
     </div>
   );
 };
